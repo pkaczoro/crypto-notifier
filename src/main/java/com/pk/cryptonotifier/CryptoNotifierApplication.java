455df730a5d922f8708e83e4662ec55656464eea
pkaczoro@gmail.com <bithub123@>
2018-11-18 12:34:50 +0100

    init

---
+++ src/main/java/com/pk/cryptonotifier/CryptoNotifierApplication.java
@@ -0,0 +1,12 @@
+package com.pk.cryptonotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoNotifierApplication.class, args);
+    }
+}
